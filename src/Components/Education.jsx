ce9366d25731f5217928a597bf9d6db0374518f6
Ravi Kishan <57055822+Ravikisha@users.noreply.github.com>
2024-02-07 19:51:58 +0000

    Updating the latest changes

---
+++ src/Components/Education.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
+            <div className="courses">
+
+            <div>
+                <h3>Full Stack Web Development</h3>
+                <h4>Udemy</h4>
+                <p>Completed a 6-month course on Full Stack Web Development. The course included HTML, CSS, JavaScript, React, Node.js, Express.js, MongoDB, and more.</p>
+            </div>
+            <div>
+                <h3>Bachelor of Computer Application</h3>
+                <h4>Prestige Institute of Management and Research</h4>
+                <p>Completed a 3-year course on Computer Application. The course included C, C++, Java, Python, and more.</p>
+            </div>
+            </div>
+        </section>
+    )
+}
+
+export default Education;
